7ce012056439be1874048ae0258155ce2dbaceaa
Max Williams <max@new-bamboo.co.uk>
2010-08-17 12:48:21 +0100

    Added all the scripts for minifying

---
+++ public/javascripts/game_complete.js
@@ -0,0 +1,18 @@
+/*  Tower
+ *  (c) 2010 Max Williams
+ *
+ *  Released under MIT license.
+ */
+//= require "AStar_memtronic"
+//= require "game"
+//= require "levels"
+//= require "units"
+//= require "grid"
+//= require "helicopter"
+//= require "grid"
+//= require "soldier"
+//= require "soldier_manager"
+//= require "explosion"
+//= require "obstacle"
+//= require "turret"
+
